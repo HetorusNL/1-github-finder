03428addf9255e77268071ad4ddc00d79fb49949
Hetorus NL <timkleinnijenhuis@hotmail.com>
2019-08-29 18:07:34 +0200

    fetch content from github api, refactored users state to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+
+    console.log(res);
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
           <h1>Hello</h1>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -9,33 +9,10 @@ class Users extends Component {
     gridGap: "1rem"
   };
 
-  state = {
-    users: [
-      {
-        login: "HetorusNL",
-        id: 17402329,
-        avatar_url: "https://avatars0.githubusercontent.com/u/17402329?v=4",
-        html_url: "https://github.com/HetorusNL"
-      },
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt"
-      }
-    ]
-  };
-
   render() {
     return (
       <div style={this.userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
