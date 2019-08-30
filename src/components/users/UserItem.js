9a2865ae08691ea33071872b5c3edde4ba198c2b
Hetorus NL <timkleinnijenhuis@hotmail.com>
2019-08-30 16:17:42 +0200

    refactored some colors in css file

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -12,7 +12,7 @@ const UserItem = ({ user: { avatar_url, html_url, login } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
+        <a href={html_url} className="btn btn-sm my-1">
           More
         </a>
       </div>
