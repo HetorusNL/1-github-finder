de0b4c40275c60816e0260348248c78910d075d9
Hetorus NL <timkleinnijenhuis@hotmail.com>
2023-03-04 18:07:28 +0100

    prepared for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "no deploy steps are performed for this repository/project!"
+echo "starting deployment of github finder"
+
+echo ""
+echo "removing existing content from folder"
+echo "sudo rm -r /data/caddy-data/github-finder.hetorus.nl/*"
+sudo rm -rf /data/caddy-data/github-finder.hetorus.nl/*
+
+echo ""
+echo "copying build github finder to the server root"
+echo "cp -r build/* /data/caddy-data/github-finder.hetorus.nl/"
+cp -r build/* /data/caddy-data/github-finder.hetorus.nl/
+
+echo ""
+echo "finished deployment of github finder!"
